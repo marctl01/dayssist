20fd77fc118e1b1f6835cc067ea87949f5a38c74
joelito24 <joelsoulange@gmail.com>
2023-03-24 16:30:56 +0100

    Feat(boostrap) -Instalar bootstrap en el proyecto

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,20 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Dashboard</h4>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">You are logged in!</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </x-app-layout>
