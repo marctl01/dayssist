3644ccbf62397d131df4c5b1e2df0539e50c25ed
Marcos Torrubia Lorca <mtorrubia42@gmail.com>
2023-04-12 09:38:05 +0200

    Feat(model) - Creacion de los modelos

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'description', 'start_date', 'finish_date'
+        // , 'id_group'
+    ];
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class, 'id_group');
+    }
 }
