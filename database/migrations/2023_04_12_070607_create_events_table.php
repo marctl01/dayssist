dc1c098fb93fa11fe97e80e4d701ae1fb4f1d165
Marcos Torrubia Lorca <mtorrubia42@gmail.com>
2023-04-12 09:33:21 +0200

    Feat(migration) - Creacion de las tablas

--- database/migrations/2023_04_12_070607_create_events_table.php
+++ database/migrations/2023_04_12_070607_create_events_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            // $table->string('rol');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('start_date');
+            $table->dateTime('finish_date');
+            // $table->unsignedBigInteger('id_groups')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('events');
     }
 };
