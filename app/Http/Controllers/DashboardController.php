60e44c6433dab61046f5645772723caa4df75e91
piotrnuria <piotrtrembecki.nuria@gmail.com>
2023-05-08 19:30:10 +0200

    Feat - Mostrar meses con bucle

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Helpers\Calendar;
+use App\Helpers\CalendarDate;
+use App\Helpers\CurrentDate;
 
 
 class DashboardController extends Controller
 {
 
-    public function index(){
-        return view('dashboard');
+    // /**
+    //  * Create a new controller instance.
+    //  *
+    //  * @return void
+    //  */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+        $calendar = new Calendar(new CurrentDate, new CalendarDate);
+
+        $calendar->setSundayFirst(false);
+        $calendar->create();
+        // $calendar->getMonthLabels();
+        // dd($calendar);
+
+        return view('dashboard', compact('calendar'));
     }
+
+    // public function index(){
+    //     return view('dashboard');
+    // }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,86 +4,28 @@
     <div class="row">
         @include('layouts.complements.sidebar')
         <div class="boxes">
-            <div class="row justify-content-center">
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Enero</span>
-                    </a>
-                </div>
-                
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Febrero</span>
-                    </a>
-                </div>
-            
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Marzo</span>
-                    </a>
-                </div>
-            </div>
+                @php
+                    $count = 0;
+                @endphp
+                    <div class="row justify-content-center">
+                        @foreach ($calendar->getMonthLabels() as $months)
+                            <div class="box" style="border: 1px solid #0000">
+                                <a href="{{ route('calendar') }}">
+                                    <span>{{$months}}</span>
+                                </a>
+                            </div>
+                            @php
+                                $count++;
+                            @endphp
 
-            <div class="row justify-content-center">
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Abril</span>
-                    </a>
-                </div>
-                
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Mayo</span>
-                    </a>
-                </div>
-            
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Junio</span>
-                    </a>
-                </div>
-            </div>
-
-            <div class="row justify-content-center">
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Julio</span>
-                    </a>
-                </div>
-                
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Agosto</span>
-                    </a>
-                </div>
-            
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Septiembre</span>
-                    </a>
-                </div>
-            </div>
-
-            <div class="row justify-content-center">
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Octubre</span>
-                    </a>
-                </div>
-                
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Noviembre</span>
-                    </a>
-                </div>
-            
-                <div class="box" style="border: 1px solid #0000">
-                    <a href="{{ route('calendar') }}">
-                        <span>Diciembre</span>
-                    </a>
-                </div>
-            </div>
-        </div>
+                            @if ($count % 3 == 0)
+                                </div><div class="row justify-content-center">
+                            @endif
+                        @endforeach
+                    </div>
+                    @if ($count % 3 != 0)
+                        </div>
+                    @endif
     </div>
 </div>
 @endsection
@@ -91,17 +33,17 @@
 <script>
     // Obtener la fecha actual usando Carbon
     var now = moment();
-    
+
     // Iterar sobre todos los meses y comparar la fecha actual
     $('.box').each(function() {
         var month = $(this).find('span').text();
         var date = moment(month + ' 1, ' + now.year(), 'MMMM D, YYYY');
-        
+
         // Agregar la clase "box-pasado" si el mes ya ha pasado
         if (now.isAfter(date, 'month')) {
             $(this).addClass('box-pasado');
         }
-        
+
         // Agregar la clase "box-activo" si el mes es el actual
         if (now.isSame(date, 'month')) {
             $(this).addClass('box-activo');
