38a4d17b5dc47af6277663a5f01c5d156fd6b48b
marctl01 <mtorrubia42@gmail.com>
2023-05-17 11:28:00 +0200

    Feat(Admin) - SlideBar Update

--- resources/views/layouts/complements/admin/users/sidebar.blade.php
+++ resources/views/layouts/complements/admin/users/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="sidebar" style="display: flex; flex-direction: column; align-items: flex-start;">
-    <a href="{{ url('/adm_users') }}" class="sidebar-link" style="margin-bottom: 5px;">Admin Usuarios</a>
+    <a href="{{ url('/adm_users') }}" class="sidebar-link" style="margin-bottom: 5px;">Ver Usuarios</a>
     <a href="{{ url('/adm_users') }}" class="sidebar-link" style="margin-bottom: 5px;">Crear Usuario</a>
     <a href="{{ url('/adm_users') }}" class="sidebar-link" style="margin-bottom: 5px;">Editar Usuario</a>
     <a href="{{ url('/adm_users') }}" class="sidebar-link" style="margin-bottom: 5px;">Eliminar Usuario</a>
